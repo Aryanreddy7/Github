89a8ca2b330101ca31461bc93093c05528c97cf2
Aryan <aryanreddy126.gmail.com>
2025-03-26 23:54:31 +0530

    Uploaded

---
+++ Assignment16/src/com/xworkz/StaticVariblesAndEncapsulation/BedSheet.java
@@ -0,0 +1,8 @@
+package com.xworkz.StaticVariblesAndEncapsulation;
+
+public class BedRunner {
+    public void clean(Bed bed){
+        bed.setSize(12);
+        System.out.println(bed.getSize());
+    }
+}
